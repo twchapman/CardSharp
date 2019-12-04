6d5cb8023e700730127918f2e689982f50deb64d
Travis Chapman <traviswchapman@gmail.com>
2019-12-04 01:37:30 -0500

    Add cast for single card to/from list of cards.

---
+++ CardSharp.Tests/CardTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace CardSharp.Tests {
+    [TestClass]
+    public class CardTests {
+        [TestMethod]
+        public void Card_SingleCard_ConvertToList() {
+            var card = new Card<int>();
+
+            CollectionAssert.AreEqual(new[] { card }, (List<Card<int>>)card);
+        }
+
+        [TestMethod]
+        public void Card_ListOf1Cards_ConvertToSingleCard() {
+            var card = new Card<int>();
+
+            Assert.AreEqual<Card<int>>(card, new List<Card<int>> { card });
+        }
+
+        [TestMethod]
+        public void Card_ListOf3CardsCard_Throws() {
+            var card = new Card<int>();
+
+            var list = new List<Card<int>> { card, card };
+
+            var exception = Assert.ThrowsException<InvalidCastException>(() => (Card<int>)list);
+            Assert.AreEqual(exception.Message, "Can only cast a List of 1 cards to a Single card, found 2 instead.");
+        }
+    }
+}
--- CardSharp/Card.cs
+++ CardSharp/Card.cs
@@ -1,4 +1,8 @@
-﻿namespace CardSharp {
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CardSharp {
     public class Card<T> {
         public Card() { }
         public Card(T data) {
@@ -6,5 +10,11 @@
         }
 
         public T Data { get; set; }
+
+        public static implicit operator List<Card<T>>(Card<T> card) => new List<Card<T>> { card };
+        public static implicit operator Card<T>(List<Card<T>> cards) {
+            if (cards.Count != 1) throw new InvalidCastException($"Can only cast a List of 1 cards to a Single card, found {cards.Count} instead.");
+            return cards.First();
+        }
     }
 }
