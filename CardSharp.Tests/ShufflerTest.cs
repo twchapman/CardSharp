f3ec4714755d70c7f1315d1d6398e8a558ea663d
Travis Chapman <traviswchapman@gmail.com>
2019-12-03 21:57:04 -0500

    Add a Shuffler class
    Hey remember that Swap method I added that one time?

---
+++ CardSharp.Tests/ShufflerTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace CardSharp.Tests {
+    [TestClass]
+    public class ShufflerTest {
+        [TestMethod]
+        public void FisherYates_GivenKnownSequence_ShufflesIntoSequence() {
+            var randomizer = new FakeRandomizer(0, 1, 2);
+            var deck = new Deck<int>(new[] { 3, 2, 1 });
+            var shuffler = new Shuffler(randomizer);
+
+            var shuffledDeck = shuffler.FisherYates(deck);
+
+            CollectionAssert.AreEqual(new[] { 1, 2, 3 }, shuffledDeck);
+        }
+    }
+}
---
+++ CardSharp/Shuffler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CardSharp {
+    public class Shuffler {
+        private readonly IRandomizer<int> randomizer;
+
+        public Shuffler(IRandomizer<int> randomizer) {
+            this.randomizer = randomizer;
+        }
+
+        public Deck<T> FisherYates<T>(Deck<T> deck) {
+            Deck<T> shuffledDeck = new Deck<T>(deck);
+            for (int i = deck.Count - 1; i > 1;  i--) {
+                int swapIndex = randomizer.Next();
+                shuffledDeck.Swap(i, swapIndex);
+            }
+            return shuffledDeck;
+        }
+    }
+}
