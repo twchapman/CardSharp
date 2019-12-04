63c77de65e3b41915ca3892e6cc2ebf8be79b8e7
Travis Chapman <traviswchapman@gmail.com>
2019-12-03 21:50:56 -0500

    Add base exception type

---
+++ CardSharp/CardSharpException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace CardSharp {
+    class CardSharpException : Exception {
+        public CardSharpException() {
+        }
+
+        public CardSharpException(string message) : base(message) {
+        }
+
+        public CardSharpException(string message, Exception innerException) : base(message, innerException) {
+        }
+
+        protected CardSharpException(SerializationInfo info, StreamingContext context) : base(info, context) {
+        }
+    }
+}
